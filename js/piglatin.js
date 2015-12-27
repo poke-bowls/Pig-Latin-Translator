10c059b628704348ad04817ace4f158ae677a70e
Micah Koki <micah.koki@gmail.com>
2015-12-26 19:24:34 -0800

    PIG LATIN BIZNATCH

--- js/piglatin.js
+++ js/piglatin.js
@@ -1,46 +1,37 @@
-//Strings: split => slice
-//split = split input into an array of substrings (each word), return new array
-//slice = extract part to append 'ay' to, push to array
-//Array:
+var eggs = 'I like steak and eggs for breakfast';
 
- var eggs = 'I like steak and eggs for breakfast';
+function translator( string ) {
+   var vowels = [ 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' ];
+   var pigLatin = [];
+   //split string into an array of substrings (each word is a string)
+   var words = string.split( ' ' );
 
- function translator( string ) {
-     var vowels = [ 'a', 'e', 'i', 'o', 'u' ];
-     var pigLatin = [];
-     //split string into an array of substrings (each word is a string)
-     var words = string.split( ' ' );
+   console.log(words);
 
-     console.log(words);
-
-     for ( var i = 0; i < words.length; i++ ) {
-      var firstLetter = words[i].charAt( 0 ).toLowerCase();
-        for(var j = 0; j < vowels.length; j++) {
-
-           //if the first letter of the word at index i of array words is a vowel
-
-           if( firstLetter === vowels[j]) {
-
-             pigLatin.push(words[i] += '-ay');
-
-           //if the string at index i of array words is longer than 1
-
-           } else if ( words[i].length > 1 ) {
-
-           //walk through the vowels and the string to find the first vowel
-
-           //walk through the string (the word at index i of array words)
-           for ( var k = 0; k < words[i].length; k++) {
-             if ( words[i].charAt(k) === vowels[j] ) {
-
-               var newString = words[i].substring(words[i].indexOf(words[i].charAt(k))) + words[i].substring( 0, words[i].indexOf(words[i].charAt(k)) ) + "-ay";
-
-               pigLatin.push( newString );
-        }
+   for ( var i = 0; i < words.length; i++ ) {
+      //walk through the vowels and the string to find the first vowel
+      outerloop:
+      for ( var k = 0; k < words[i].length; k++) {
+        var firstLetter = words[i].charAt( 0 ).toLowerCase();
+        for ( var j = 0; j < vowels.length; j++ ) {
+          if( firstLetter === vowels[j] && words[i].length > 1 ){
+            pigLatin.push( words[i] += '-ay' );
+            break outerloop;
+          } else if ( firstLetter === vowels[j] && words[i].length === 1 ) {
+            pigLatin.push( words[i] += '-ay' );
+            break outerloop;
+          } else if ( words[i].charAt(k) === vowels[j] ) {
+              console.log(words[i].charAt(k));
+              var newString = words[i].substring(words[i].indexOf(words[i].charAt(k))) + "-" + words[i].substring( 0, words[i].indexOf(words[i].charAt(k)) ) + "ay";
+              pigLatin.push( newString );
+              break outerloop;
+          } else if ( words[i].charAt( 0 ) === vowels[j] ){
+            pigLatin.push(words[0] += 'ay');
+            break outerloop;
+          }
         }
       }
     }
-  }
-     return pigLatin.join(' ');
- }
- translator(eggs);
+   return pigLatin.join(' ');
+}
+translator(eggs);
