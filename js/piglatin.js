fb4ec9c6fe7fe31aa6a87d014e8ef75af8798492
Micah Koki <micah.koki@gmail.com>
2015-12-26 18:50:30 -0800

    Pig Latin translating, need work on newString

--- js/piglatin.js
+++ js/piglatin.js
@@ -6,7 +6,7 @@
  var eggs = 'I like steak and eggs for breakfast';
 
  function translator( string ) {
-     var vowels = [ 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' ];
+     var vowels = [ 'a', 'e', 'i', 'o', 'u' ];
      var pigLatin = [];
      //split string into an array of substrings (each word is a string)
      var words = string.split( ' ' );
@@ -14,25 +14,33 @@
      console.log(words);
 
      for ( var i = 0; i < words.length; i++ ) {
-        //if the string at index i of array words is longer than 1
-         if( words[i].length > 1 ){
-            //walk through the vowels and the string to find the first vowel
-            outerloop:
-            for ( var k = 0; k < words[i].length; k++) {
-                for ( var j = 0; j < vowels.length; j++ ) {
-                    if ( words[i].charAt(k) === vowels[j] ) {
-                        console.log(words[i].charAt(k));
-                        var newString = words[i].substring(words[i].indexOf(words[i].charAt(k))) + words[i].substring( 0, words[i].indexOf(words[i].charAt(k)) ) + "ay";
-                        pigLatin.push( newString );
-                        break outerloop;
-                    } else if ( words[i].charAt( 0 ) === vowels[j] ){
-                      pigLatin.push(words[0] += 'ay');
-                      break outerloop;
-                    }
-                }
-            }
-         }
-     }
+      var firstLetter = words[i].charAt( 0 ).toLowerCase();
+        for(var j = 0; j < vowels.length; j++) {
+
+           //if the first letter of the word at index i of array words is a vowel
+
+           if( firstLetter === vowels[j]) {
+
+             pigLatin.push(words[i] += '-ay');
+
+           //if the string at index i of array words is longer than 1
+
+           } else if ( words[i].length > 1 ) {
+
+           //walk through the vowels and the string to find the first vowel
+
+           //walk through the string (the word at index i of array words)
+           for ( var k = 0; k < words[i].length; k++) {
+             if ( words[i].charAt(k) === vowels[j] ) {
+
+               var newString = words[i].substring(words[i].indexOf(words[i].charAt(k))) + words[i].substring( 0, words[i].indexOf(words[i].charAt(k)) ) + "-ay";
+
+               pigLatin.push( newString );
+        }
+        }
+      }
+    }
+  }
      return pigLatin.join(' ');
  }
  translator(eggs);
