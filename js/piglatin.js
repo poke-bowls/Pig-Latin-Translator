ae899b3c5e1ce53b57e649c7b64237dcac48d05c
Micah Koki <micah.koki@gmail.com>
2015-12-26 21:19:36 -0800

    Added case for 'y'

--- js/piglatin.js
+++ js/piglatin.js
@@ -1,7 +1,7 @@
-var eggs = 'I like steak and eggs for breakfast';
+var eggs = 'Feel the rhythm and the yucky yellow xylophone';
 
 function translator( string ) {
-   var vowels = [ 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' ];
+   var vowels = [ 'a', 'e', 'i', 'o', 'u', 'y' ];
    var pigLatin = [];
    //split string into an array of substrings (each word is a string)
    var words = string.split( ' ' );
@@ -14,20 +14,18 @@ function translator( string ) {
       for ( var k = 0; k < words[i].length; k++) {
         var firstLetter = words[i].charAt( 0 ).toLowerCase();
         for ( var j = 0; j < vowels.length; j++ ) {
-          if( firstLetter === vowels[j] && words[i].length > 1 ){
-            pigLatin.push( words[i] += '-ay' );
-            break outerloop;
-          } else if ( firstLetter === vowels[j] && words[i].length === 1 ) {
+          if( firstLetter === vowels[j] ){
             pigLatin.push( words[i] += '-ay' );
             break outerloop;
           } else if ( words[i].charAt(k) === vowels[j] ) {
-              console.log(words[i].charAt(k));
-              var newString = words[i].substring(words[i].indexOf(words[i].charAt(k))) + "-" + words[i].substring( 0, words[i].indexOf(words[i].charAt(k)) ) + "ay";
-              pigLatin.push( newString );
-              break outerloop;
-          } else if ( words[i].charAt( 0 ) === vowels[j] ){
-            pigLatin.push(words[0] += 'ay');
+            var newString = words[i].substring(words[i].indexOf(words[i].charAt(k))) + "-" + words[i].substring( 0, words[i].indexOf(words[i].charAt(k)) ) + "ay";
+            pigLatin.push( newString );
             break outerloop;
+          } else if ( words[i].charAt( 0 ) === 'y' ) {
+              vowels = [ 'a', 'e', 'i', 'o', 'u' ];
+              var special = words[i].substring(words[i].indexOf(words[i].charAt(k + 1))) + "-" + words[i].substring( 0, words[i].indexOf(words[i].charAt(k)) ) + "yay";
+              pigLatin.push( special );
+              break outerloop;
           }
         }
       }
